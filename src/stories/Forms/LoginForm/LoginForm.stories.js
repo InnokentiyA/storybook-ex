e4f1d5b2cd2fb49985e7347617424f12a4ebadd3
itykhun <itykhun@griddynamics.com>
2023-05-05 10:57:02 +0300

    extend LoginForm.stories.js

--- src/stories/Forms/LoginForm/LoginForm.stories.js
+++ src/stories/Forms/LoginForm/LoginForm.stories.js
@@ -109,10 +109,17 @@ export const ExampleLoginWithBug = {
 };
 export const ExampleApi = () => {
   setupApiMock();
+  const expectedResponse = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+  ];
 
   // Call example API endpoint
   axios.get('/api/users').then((response) => {
     console.log(response.data);
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual(expectedResponse);
   });
 
   return <div>Check the console for API response</div>;
